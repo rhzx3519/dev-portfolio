a6a3092d2ff5263b86a07cfcfe37778533ee49c2
Zhenghao Lou <rhzx3519@gmail.com>
2024-03-18 21:54:48 +1100

    fix: resume.jsx

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,20 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Fade from 'react-reveal';
-import { Container } from '@mui/material';
 import Header from './Header';
 
-
 function Resume(props) {
-  // const theme = useContext(ThemeContext);
   const { header } = props;
   return (
     <>
       <Header title={header} />
-      <Container maxWidth="lg">
-        <Fade>
-        </Fade>
-      </Container>
     </>
   );
 }
